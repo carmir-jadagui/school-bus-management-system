ee3e2c7c3fa4cdc807cc03b0fc1125302a61da88
Carmir Jadagui <carmirjadagui@gmail.com>
2025-08-15 12:51:57 -0300

    Se crear el script inicial, para crear la BD y una tabla de Test

---
+++ database/001 - Script inicial.sql
@@ -0,0 +1,14 @@
+-- Script inicial para crear la BD y las tablas
+-- Crear la base de datos si no existe
+CREATE DATABASE IF NOT EXISTS sbms;
+
+-- Usar la base de datos
+USE sbms;
+
+-- Crea una tabla con registro de prueba, para validar la conexión entre las diferentes capastest
+CREATE TABLE `sbms`.`test` (
+  `Id` INT NOT NULL AUTO_INCREMENT,
+  `Message` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`Id`));
+
+INSERT INTO `sbms`.`test` (`Message`) VALUES ('Hola mundo desde la BD');

df25f5cff077496529295925b9f5209dac7c2d9c
Carmir Jadagui <carmirjadagui@gmail.com>
2025-08-22 03:01:52 -0300

    Mejoras en la estética de la consulta de chicos, así como también la inclusión de la columna de acciones y el servicio para eliminar

--- frontend/src/app/core/services/boyServices.ts
+++ frontend/src/app/core/services/boyServices.ts
@@ -1,21 +1,32 @@
+// Librerias de angular
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+// Otros
 import { Observable } from 'rxjs';
-import { ResultModel } from '../models/resultModel';
 import { environment } from '../../../environments/environments';
+// Modelos
+import { ResultModel } from '../models/resultModel';
+import { ResponseBaseModel } from '../models/ResponseBaseModel';
 import { BoyModel } from '../models/boyModel';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class BoyService {
   private baseUrl = environment.apiUrlBase + "boy";
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   getBoysAll(): Observable<ResultModel<BoyModel[]>> {
     return this.http.get<ResultModel<BoyModel[]>>(
       `${this.baseUrl}`
+    );
+  }
+
+  deleteBoy(id: number): Observable<ResultModel<ResponseBaseModel>> {
+    return this.http.delete<ResultModel<ResponseBaseModel>>(
+      `${this.baseUrl}/${id}`
     );
   }
 }

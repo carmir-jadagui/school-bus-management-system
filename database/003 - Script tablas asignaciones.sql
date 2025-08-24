3b6518dabf4a7101788b7e73702c038137132038
Carmir Jadagui <carmirjadagui@gmail.com>
2025-08-24 16:58:29 -0300

    se crea el script 003 - Script tablas asignaciones para crear las tablas de buses_boys y buses_drivers. Se modifica 002 - Script tablas maestro para eliminar el primary key de los dni y plate

---
+++ database/003 - Script tablas asignaciones.sql
@@ -0,0 +1,26 @@
+-- Script para crear las tablas de asignaciones
+  -- Tabla de relación micro-chicos
+  CREATE TABLE `sbms`.`buses_boys` (
+    `BusId` INT NOT NULL,
+    `BoysId` INT NOT NULL,
+    PRIMARY KEY (`BusId`, `BoysId`),
+    CONSTRAINT `FK_Buses_Boys`
+      FOREIGN KEY (`BusId`)
+      REFERENCES `sbms`.`buses` (`Id`),
+    CONSTRAINT `FK_Boys_Buses`
+      FOREIGN KEY (`BoysId`)
+      REFERENCES `sbms`.`boys` (`Id`)
+  );
+
+  -- Tabla de relación micro-choferes
+  CREATE TABLE `sbms`.`buses_drivers` (
+    `BusId` INT NOT NULL,
+    `DriversId` INT NOT NULL,
+    PRIMARY KEY (`BusId`, `DriversId`),
+    CONSTRAINT `FK_Buses_Drivers`
+      FOREIGN KEY (`BusId`)
+      REFERENCES `sbms`.`buses` (`Id`),
+    CONSTRAINT `FK_Drivers_Buses`
+      FOREIGN KEY (`DriversId`)
+      REFERENCES `sbms`.`drivers` (`Id`)
+  );

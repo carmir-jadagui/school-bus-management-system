2b7b10cb2db6190d13ca6f1edd698a4b2c2078e9
Carmir Jadagui <carmirjadagui@gmail.com>
2025-08-24 23:36:00 -0300

    Se agregan las columnas chofer y cant de chicos, así como también el botón con la acción de detalles

--- frontend/src/app/core/models/busModel.ts
+++ frontend/src/app/core/models/busModel.ts
@@ -1,5 +1,10 @@
+import { BoyModel } from "./boyModel";
+import { DriverModel } from "./driverModel";
+
 export interface BusModel {
   id: number;
   plate: string;
   brand: string;
+  driver: DriverModel;
+  boys: BoyModel[];
 }

9e88d43325be328ce297eb5fdd6d339311cf8752
Carmir Jadagui <carmirjadagui@gmail.com>
2025-08-24 21:23:09 -0300

    Se crea método privado AddBusAssignments para guardar las relaciones y ser usado tanto para el create como el update. Se modifica el update para que sea transaccional y elimine primero las posibles asignaciones antes de guardar las nuevas

--- backend/src/infrastructure/persistence/SBMS.Persistence.MySQL/Repositories/BusRepository.cs
+++ backend/src/infrastructure/persistence/SBMS.Persistence.MySQL/Repositories/BusRepository.cs
@@ -140,35 +140,8 @@
 
                 result.Id = bus.Id;
 
-                // Para guardar la relación entre el micro y el chofer
-                if (busModel.Driver != null)
-                {
-                    var busDriver = new BusesDriver()
-                    {
-                        BusId = bus.Id,
-                        DriversId = busModel.Driver.Id
-                    };
-
-                    _dbContext.BusesDrivers.Add(busDriver);
-                };
-
-                // Para guardar la relación entre el micro y el(los) chico(a)(s)
-                if (busModel.Boys?.Count > 0)
-                {
-                    foreach (var boy in busModel.Boys)
-                    {
-                        var busBoy = new BusesBoy()
-                        {
-                            BusId = bus.Id,
-                            BoysId = boy.Id
-                        };
-
-                        _dbContext.BusesBoys.Add(busBoy);
-                    }
-                };
-
-                // Guardar las relaciones
-                await _dbContext.SaveChangesAsync();
+                // Para guardar las posibles asignaciones
+                await AddBusAssignments(busModel);
 
                 // Commit de la transacción
                 await transaction.CommitAsync();
@@ -178,32 +151,65 @@
             catch (Exception ex)
             {
                 await transaction.RollbackAsync();
+
                 throw new SBMSPersistenceException("Persistence Layer Failure: CreateBus", ex.Message);
             }
         }
 
         public async Task<ResponseBaseModel> UpdateBus(BusModel busModel)
         {
+            // se aplica Transaction / Rollback
+            using var transaction = await _dbContext.Database.BeginTransactionAsync();
+
             var result = new ResponseBaseModel();
 
             try
             {
                 var bus = await _dbContext.Buses.FirstOrDefaultAsync(x => x.Id == busModel.Id);
 
+                // Si encutra el micro
                 if (bus != null)
                 {
+                    // Modifica los datos de micro
                     bus.Brand = busModel.Brand;
                     bus.UpdatedAt = DateTime.Now;
 
+                    result.Id = bus.Id;
+
+
+                    // Elimana las posibles asignaciones previas
+                    // Elimina la asignación del chofer
+                    var driver = await _dbContext.BusesDrivers.FirstOrDefaultAsync(x => x.BusId == result.Id);
+
+                    if (driver != null)
+                    {
+                        _dbContext.BusesDrivers.Remove(driver);
+                    }
+
+                    // Elimina las asignaciones de los chicos
+                    var boys = await _dbContext.BusesBoys.Where(x => x.BusId == result.Id).ToListAsync();
+
+                    if (boys.Count > 0)
+                    {
+                        _dbContext.BusesBoys.RemoveRange(boys);
+                    }
+
+                    // Para guardar la modificación de los datos del micro y eliminar las posibles asignaciones
                     await _dbContext.SaveChangesAsync();
 
-                    result.Id = bus.Id;
+                    // Para guardar las posibles asignaciones
+                    await AddBusAssignments(busModel);
                 }
+
+                // Commit de la transacción
+                await transaction.CommitAsync();
 
                 return result;
             }
             catch (Exception ex)
             {
+                await transaction.RollbackAsync();
+
                 throw new SBMSPersistenceException("Persistence Layer Failure: UpdateBus", ex.Message);
             }
         }
@@ -230,6 +236,40 @@
             {
                 throw new SBMSPersistenceException("Persistence Layer Failure: DeleteBus", ex.Message);
             }
+        }
+
+        // Guarda la asignación del chofer y de(los) chico(a)(s), si fuese el caso
+        private async Task AddBusAssignments(BusModel busModel)
+        {
+            // Para guardar la relación entre el micro y el chofer
+            if (busModel.Driver != null)
+            {
+                var busDriver = new BusesDriver()
+                {
+                    BusId = busModel.Id,
+                    DriversId = busModel.Driver.Id
+                };
+
+                _dbContext.BusesDrivers.Add(busDriver);
+            };
+
+            // Para guardar la relación entre el micro y el(los) chico(a)(s)
+            if (busModel.Boys?.Count > 0)
+            {
+                foreach (var boy in busModel.Boys)
+                {
+                    var busBoy = new BusesBoy()
+                    {
+                        BusId = busModel.Id,
+                        BoysId = boy.Id
+                    };
+
+                    _dbContext.BusesBoys.Add(busBoy);
+                }
+            };
+
+            // Guardar las relaciones
+            await _dbContext.SaveChangesAsync();
         }
     }
 }
